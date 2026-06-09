5d705f4031ca457720e5ea33c93f2470e69aa51e
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-06-09 19:56:15 +0300

    Create part-y-0095-char-indices.rs

---
+++ part-y-0095-char-indices.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+    let my_name = "mustafa";
+
+    for (p, c) in my_name.char_indices() {
+
+        println!("Char index: {p} and char: {c}")
+    }
+}
+
+// Char index: 0 and char: m
+// Char index: 1 and char: u
+// Char index: 2 and char: s
+// Char index: 3 and char: t
+// Char index: 4 and char: a
+// Char index: 5 and char: f
+// Char index: 6 and char: a
